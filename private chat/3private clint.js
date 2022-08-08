dcba9d6011bae01dc2bec260db97494c0f212972
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-08-09 00:15:56 +0500

    private chat test

--- private chat/3private clint.js
+++ private chat/3private clint.js
@@ -1,11 +1,21 @@
 const io = require("socket.io-client");
-const socket = io("https://mix-chat-1.herokuapp.com")
+// const socket = io("https://mix-chat-1.herokuapp.com")
+const socket = io("http://127.0.0.1:3300")
 var uname, friend_name;
 console.log("welcome");
 socket.on("connect", () => {
    console.log(socket.id);
-   socket.emit("register", "rana");
+   socket.emit("register", "+tayyab");
   });
+
+function sendMessage(user,friend,message) { 
+    socket.emit('private_chat', {
+        user: user,
+        friend: friend,
+        message: message
+    });
+}
+sendMessage("+tayyab","+abobakar","i am tayyab");
 
 socket.on('private_chat', function (msg) {
    console.log(msg);
--- private chat/4private clint.js
+++ private chat/4private clint.js
@@ -1,11 +1,22 @@
 const io = require("socket.io-client");
-const socket = io("https://mix-chat-1.herokuapp.com")
+// const socket = io("https://mix-chat-1.herokuapp.com")
+const socket = io("http://127.0.0.1:3300")
 var uname, friend_name;
 console.log("welcome");
 socket.on("connect", () => {
    console.log(socket.id);
-   socket.emit("register", "raja");
+   socket.emit("register", "+abobakar");
   });
+
+function sendMessage(user,friend,message) {
+ 
+    socket.emit('private_chat', {
+        user: user,
+        friend: friend,
+        message: message
+    });
+}
+sendMessage("+abobakar","+tayyab","i am abobakar");
 
 socket.on('private_chat', function (msg) {
    console.log(msg);
